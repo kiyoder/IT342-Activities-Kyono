943f57d2f5de81363e11cc0ee33b5faf614e9221
LAB14_USER ON WS30 <L14X20W30@citulab.net>
2025-02-21 21:01:47 +0800

    OAuth2Login for Google and Github

--- g7-kyono-yoshinori/oauth2login/src/main/java/com/kyono/oauth2login/Oauth2loginApplication.java
+++ g7-kyono-yoshinori/oauth2login/src/main/java/com/kyono/oauth2login/Oauth2loginApplication.java
@@ -3,6 +3,12 @@ package com.kyono.oauth2login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
 @SpringBootApplication
 public class Oauth2loginApplication {
 
